58c11e03dac2c372057ca6570c7bae741979d7a8
Andre <andre.hansen146@gmail.com>
2024-04-04 22:16:53 +0200

    inital commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'restaurant_utsyn_database'
+})
+
+app.get('/', (req,res) => 
+{
+    return res.json("Resturant-utsyn databse");
+});
+
+//eksempel på querry
+app.get('/bruker', (req, res)=>{
+    //det var så langt jeg kom
+})
+
+app.listen(8001, ()=>
+{
+    console.log("listening");
+});
